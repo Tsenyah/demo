7ffe9b54fd6f419a503d488c5d78031045ff36cc
Tsenyah <thomassenyah@msn.com>
2023-11-08 13:31:44 +0000

    processor

--- docker-tutorial/src/server.js
+++ docker-tutorial/src/server.js
@@ -6,6 +6,7 @@ app.get('/',(req,res)=>{
     res.send("arch: "+ os.arch())
 })
 app.listen(3000, function (){
-    console.log("app listening on port 3000")
+    console.log(process.arch)
+    console.log("app listening on port 3000 maybe?")
     console.log("arch: "+ os.arch())
 })
