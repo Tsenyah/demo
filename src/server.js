464d28d2bfbf41b1226d4d404732c950591c0442
Tsenyah <thomassenyah@msn.com>
2023-11-08 14:56:40 +0000

    refactored repo

--- src/server.js
+++ src/server.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const app = express();
+const os = require('os');
 
 app.get('/',(req,res)=>{
-    res.send("Welcome to my other awesome App!")
+    res.send("arch: "+ os.arch())
 })
 app.listen(3000, function (){
-    console.log("app listening on port 3000")
+    console.log(process.arch)
+    console.log("app listening on port 3000 maybe?")
+    console.log("arch: "+ os.arch())
 })
