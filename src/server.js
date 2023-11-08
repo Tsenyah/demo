cd63c8461df6a61b1485a5aa6bd5048dfd09d945
Tsenyah <thomassenyah@msn.com>
2023-11-08 14:58:33 +0000

    main fix

--- src/server.js
+++ src/server.js
@@ -1,12 +1,9 @@
 const express = require('express');
 const app = express();
-const os = require('os');
 
 app.get('/',(req,res)=>{
-    res.send("arch: "+ os.arch())
+    res.send("Welcome to my other awesome App!")
 })
 app.listen(3000, function (){
-    console.log(process.arch)
-    console.log("app listening on port 3000 maybe?")
-    console.log("arch: "+ os.arch())
+    console.log("app listening on port 3000")
 })
